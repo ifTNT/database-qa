07e91a9561833cc1195c25c31ac6b3f37b554c10
Yung-Hsiang Hu <iftnt1999@gmail.com>
2023-10-14 18:15:29 +0800

    new: Script to construct vector database
    This patch contributes adds a script to construct the FAISS vector
    database from local documents.

---
+++ README.md
@@ -0,0 +1,29 @@
+DataBase Question Answering (DBQA)
+===
+DBQA is an application that utilizes Retrieval-Augmented Generation (RAG) to answer the question related to local documents.
+
+## Feature
+
+- Support various document format. e.g. txt, html, word, pdf, epub, ...
+
+## Dependency Installation
+### For Ubuntu
+
+```bash
+apt-get install python-dev libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext
+pip install textract
+```
+
+### For Archlinux
+
+```bash
+pacman -S python libxml2 libxslt antiword unrtf poppler pstotext
+pip install textract
+```
+
+## Usage
+
+1. Construct the vector database from local documents
+    ```bash
+    python construct_vector_db.py /path/to/documents /path/to/output/directory
+    ```
---
+++ construct_vector_db.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import argparse
+import logging
+from dotenv import load_dotenv, find_dotenv
+from langchain.document_loaders import DirectoryLoader
+
+from lib.gpu_util import check_gpu
+from lib.textract_loader import TextractLoader
+from lib.parallel_splitter import ParallelSplitter
+from lib.document_store import DocumentStore
+
+logger = logging.getLogger(__name__)
+
+def construct_db(docs_path: str, output_path: str):
+    """
+    Construct vector database from local documents and save to the destination.
+    """
+
+    loader = DirectoryLoader(docs_path,
+                         recursive=True,
+                         loader_cls=TextractLoader,
+                         use_multithreading=True,
+                         show_progress=True)
+    logger.info(f'Loading documents...')
+    docs = loader.load()
+    logger.info(f'Loaded {len(docs)} documents.')
+
+    logger.info(f'Chunking documents...')
+    splitter = ParallelSplitter(chunk_size=512, chunk_overlap=128)
+    chunks = splitter.split(docs)
+    logger.info(f'Chunked documents into {len(chunks)} chunks.')
+
+    db = DocumentStore()
+    logger.info(f'Constructing vector store...')
+    db.from_documents(chunks)
+    logger.info(f'Vector store constructed.')
+    db.save(output_path)
+    logger.info(f'Saved vector store to {output_path}.')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Construct a FAISS vector database from local documents.')
+    parser.add_argument("docs_path", help="the path to the directory of input documents.", type=str)
+    parser.add_argument("output_path", help="the path where the final database will be stored.", type=str)
+    parser.add_argument("--log", help="the log level. (INFO, DEBUG, ...)", type=str, default="INFO")
+    args = parser.parse_args()
+    
+    # Setup logger
+    numeric_level = getattr(logging, args.log.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f'Invalid log level: {args.log}')
+    logging.basicConfig(level=numeric_level)
+    
+    # Read local .env file
+    load_dotenv(find_dotenv())
+    check_gpu()
+    
+    construct_db(args.docs_path, args.output_path)
